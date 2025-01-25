831ff09cede325437bd15529ae5ef335181ab60d
Inobat <inobatmirahmedova@gmail.com>
2025-01-25 10:01:02 +0500

    button removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import { Button } from '@shadcn/ui';
 
 function App() {
   return (
     <>
-       {/* <Button className="bg-blue-500 text-white">
-        Hello ShadCN UI
-      </Button> */}
       <h1>hello world</h1>
     </>
   );
