5823c8093a23f34f5ed9e80191271672f35397f0
shokhsanam18 <shokhsanam18@gmail.com>
2025-01-25 13:35:41 +0500

    feat

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,65 @@
 import React from "react";
-import { Mail } from "lucide-react";
+import K from "../assets/simple logo for the word elevate with a rocket in blue.png";
 import { Bell } from "lucide-react";
-import { Layers } from "lucide-react";
+
 import H from "../assets/profile.jpg";
 
 export const Navbar = () => {
+  const categories = [
+    {
+      name: "Music",
+    },
+    {
+      name: "Robots",
+    },
+    {
+      name: "Technology",
+    },
+    {
+      name: "Art",
+    },
+    {
+      name: "Economics",
+    },
+    {
+      name: "Food",
+    },
+    {
+      name: "Apps",
+    },
+  ];
+
   return (
-    <nav className="flex border border-2">
-      <div>
-        <input type="search" name="input" id="Input" />
-      </div>
-      <div>
-        <button>
-          <Mail />
-        </button>
-        <button>
-          <Bell />
-        </button>
-        <button>
-          <Layers />
-        </button>
-        <div>
-          <div className="flex h-">
-            <img src={H} alt="" className="h-" />
-            <p>Hi, Naruto</p>
+    <nav className="flex  bg-white sticky justify-center items-center h-[100px] gap-3 p-2 shadow-sm w-screen flex-col">
+      <div className="flex gap-3 items-center w-full p-1 justify-between">
+        <div className="flex justify-center items-center">
+          <img src={K} alt="" className="" />
+          <h1 className="text-lg font-bold">Elevate</h1>
+        </div>
+        <div className=" ">
+          <input
+            type="search"
+            className="bg-grey-100 w-[400px] h-[40px] rounded-md border-0.5 border-black shadow-md p-2"
+          />
+        </div>
+        <div className="flex gap-2">
+          <button className="rounded-full bg-gray-200 p-2">
+            <Bell />
+          </button>
+          <div>
+            <div className="flex gap-2 items-center justify-center">
+              <img src={H} alt="" className="rounded-full h-10 w-10" />
+              <p>Hi, Naruto</p>
+            </div>
           </div>
         </div>
+      </div>
+      <div className="flex justify-center items-center gap-2">
+        {categories.map((category, index) => (
+          <button key={index} className=" p-1 rounded-md ">
+            {category.name}
+          </button>
+        ))}
       </div>
     </nav>
   );
